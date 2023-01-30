42b02db28d1e8c49f44862331e385ff72fafbd8c
KevinMora47 <kevinmora47@gmail.com>
2023-01-29 18:16:15 -0600

    API Config

--- ask-question.js
+++ ask-question.js
@@ -1,12 +1,13 @@
 import cohere from 'cohere-ai';
+
 cohere.init('bwcARddVkamySvAt3HroNuwE6Zu4TD1R8bjuV3py');
-async function aiResponse() {
+
+export async function askQuestion(input) {
   const response = await cohere.generate({
     model: 'xlarge',
-    prompt:
-      'Write 5 titles for a blog ideas for the keywords "large language model" or "text generation"',
-    max_tokens: 300,
-    temperature: 0.9,
+    prompt: input,
+    max_tokens: 500,
+    temperature: 0.3,
     k: 0,
     p: 0.75,
     frequency_penalty: 0,
@@ -14,7 +15,8 @@ async function aiResponse() {
     stop_sequences: [],
     return_likelihoods: 'NONE',
   });
-  return response.body.generations[0].text;
-}
 
-export { aiResponse };
+  const cleanText = response.body.generations[0].text;
+  console.log(cleanText);
+  return cleanText.replace('--', ' ');
+}
--- index.js
+++ index.js
@@ -1,22 +1,22 @@
 import express from 'express';
-import { response } from '../askmelink/ask-question';
+import { askQuestion } from './ask-question.js';
 const app = express();
 
-// set the view engine to ejs
 app.set('view engine', 'ejs');
 
-// use res.render to load up an ejs view file
-
-// index page
 app.get('/', async function (req, res) {
   res.render('pages/index');
-  console.log(response());
 });
 
-// about page
-app.get('/about', function (req, res) {
-  res.render('pages/about');
+app.get('/askMe', async function (req, res) {
+  const questionAsked = req.query.question;
+  const answer = await askQuestion(questionAsked);
+
+  const request = {
+    question: questionAsked,
+    answer: answer,
+  };
+  res.render('pages/answer', { request });
 });
 
 app.listen(8080);
-console.log('Server is listening on port 8080');
